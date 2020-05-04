a145f74801314e60d6b9e5027a6afd69d5ac8745
Stijn Sagaert <stijn.sagaert@outlook.com>
2020-05-04 20:46:07 +0200

    Upgrade to serverless

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-include "presentation/index.phtml";
-?>
+
+$url = explode("/",$_SERVER['REQUEST_URI']);
+$url = $url[count($url)-1];
+
+switch ($url){
+    case 'robots.txt':
+        header("Location: https://www.whereiscl.com/assets/robots.txt");
+        break;
+    case 'donate.php':
+        include "donate.php";
+        break;
+    case 'sitemap.xml':
+        header("Location: https://www.whereiscl.com/assets/sitemap.xml");
+        break;
+    default:
+        include "presentation/index.phtml";
+}
